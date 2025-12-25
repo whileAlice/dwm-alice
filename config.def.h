3adfa91688e50914e2816d25907833a573bf2422
Lady Alice <althea@disroot.org>
2025-12-25 11:46:41 +0100

    set isterminal = 1 for st to enable swallow

--- config.def.h
+++ config.def.h
@@ -261,8 +261,9 @@ static const Rule rules[] = {
 	RULE(.wintype = WTYPE "UTILITY", .isfloating = 1)
 	RULE(.wintype = WTYPE "TOOLBAR", .isfloating = 1)
 	RULE(.wintype = WTYPE "SPLASH", .isfloating = 1)
-	RULE(.class = "Gimp", .tags = 1 << 4)
-	RULE(.class = "Firefox", .tags = 1 << 7)
+	RULE(.class = "st", .isterminal = 1)
+	// RULE(.class = "Gimp", .tags = 1 << 4)
+	// RULE(.class = "Firefox", .tags = 1 << 7)
 };
 
 /* Bar rules allow you to configure what is shown where on the bar, as well as
