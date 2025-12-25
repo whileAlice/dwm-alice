699c4723e874d7af61cf5479e23f9ec4589188ba
Lady Alice <althea@disroot.org>
2025-12-25 11:46:19 +0100

    change ICONSIZE and ICONSPACING

--- config.def.h
+++ config.def.h
@@ -10,8 +10,8 @@ static const unsigned int snap           = 32;  /* snap pixel */
 static const int swallowfloating         = 1;   /* 1 means swallow floating windows by default */
 static const int showbar                 = 1;   /* 0 means no bar */
 static const int topbar                  = 1;   /* 0 means bottom bar */
-#define ICONSIZE 20    /* icon size */
-#define ICONSPACING 5  /* space between icon and title */
+#define ICONSIZE 14    /* icon size */
+#define ICONSPACING 6  /* space between icon and title */
 /* Status is to be shown on: -1 (all monitors), 0 (a specific monitor by index), 'A' (active monitor) */
 static const int statusmon               = 'A';
 
